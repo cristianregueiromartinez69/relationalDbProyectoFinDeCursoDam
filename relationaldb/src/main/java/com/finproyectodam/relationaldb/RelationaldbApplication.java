d038ee1faf3626db3b118eed2f9e8bee81762c65
cristian <cregueiromartinez@danielcastelao.org>
2025-01-26 11:02:24 +0100

    commit inicial de la aplicacion

---
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/RelationaldbApplication.java
@@ -0,0 +1,15 @@
+package com.finproyectodam.relationaldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationaldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationaldbApplication.class, args);
+	}
+
+	//TODO: crear base de datos donde se registran los usuarios
+
+}
---
+++ relationaldb/src/test/java/com/finproyectodam/relationaldb/RelationaldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finproyectodam.relationaldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationaldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
