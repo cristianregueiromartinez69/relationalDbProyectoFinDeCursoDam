7562084a8e817ee2c17568f9aefa472421ea8e1b
Cristian <cregueiromartinez@danielcastelao.org>
2025-02-10 08:53:23 +0100

    creando interfaz del repositorio de usuarios

---
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/repository/UsuariosRepository.java
@@ -0,0 +1,10 @@
+package com.finproyectodam.relationaldb.repository;
+
+import com.finproyectodam.relationaldb.model.entitys.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuariosRepository extends JpaRepository<Usuario, Long> {
+
+}
