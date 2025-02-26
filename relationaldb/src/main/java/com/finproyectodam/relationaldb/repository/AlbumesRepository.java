b9760a1e0ea9e20bb0d3f366e35c9686d555f6ba
cristian <cregueiromartinez@danielcastelao.org>
2025-02-26 16:57:32 +0100

    metodos repositorio hechos

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/repository/AlbumesRepository.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/repository/AlbumesRepository.java
@@ -4,6 +4,8 @@ import com.finproyectodam.relationaldb.model.entitys.Album;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Repositorio de albumes
  * @author cristian && Joel
@@ -15,5 +17,12 @@ public interface AlbumesRepository extends JpaRepository<Album, Integer> {
     @EntityGraph(attributePaths = {"canciones"})
     Album findByid(Integer id);
 
+    //metodo para encontrar albumes por nombre
+    @EntityGraph(attributePaths = {"canciones"})
+    Album findBytitulo(String nombre);
+
+    //metodo para encontrar albumes entre años de lanzamiento
+    @EntityGraph(attributePaths = {"canciones"})
+    List<Album> findByanolanzBetween(Integer anoInit, Integer anoFin);
 
 }
