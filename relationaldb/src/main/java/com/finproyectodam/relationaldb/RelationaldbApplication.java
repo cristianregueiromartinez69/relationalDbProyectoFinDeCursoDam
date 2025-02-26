8dd17c0bab99bfa88fac7990d998791684df669a
cristian <cregueiromartinez@danielcastelao.org>
2025-02-26 14:46:15 +0100

    hacer filtros de albumes

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/RelationaldbApplication.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/RelationaldbApplication.java
@@ -15,6 +15,6 @@ public class RelationaldbApplication {
 		SpringApplication.run(RelationaldbApplication.class, args);
 	}
 
-	//TODO: búsqueda de artista por nombre
+	//TODO: hacer filtros de albumes
 
 }
