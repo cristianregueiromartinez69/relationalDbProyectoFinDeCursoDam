91f57b68247e214defd9c77055da7cbe91cee84f
Cristian <cregueiromartinez@danielcastelao.org>
2025-03-07 10:55:04 +0100

    metodo tabla intermedia borrado de cancion de playlist

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/RelationaldbApplication.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/RelationaldbApplication.java
@@ -15,6 +15,6 @@ public class RelationaldbApplication {
 		SpringApplication.run(RelationaldbApplication.class, args);
 	}
 
-	//TODO: vamos a eliminar canciones de playlist
+
 
 }
--- relationaldb/src/main/java/com/finproyectodam/relationaldb/repository/PlaylistCancionRepository.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/repository/PlaylistCancionRepository.java
@@ -1,5 +1,6 @@
 package com.finproyectodam.relationaldb.repository;
 
+import com.finproyectodam.relationaldb.model.entitys.Cancion;
 import com.finproyectodam.relationaldb.model.entitys.Playlist;
 import com.finproyectodam.relationaldb.model.entitys.PlaylistCancion;
 import com.finproyectodam.relationaldb.model.entitys.PlaylistCancionId;
@@ -12,4 +13,11 @@ public interface PlaylistCancionRepository extends JpaRepository<PlaylistCancion
      * @param playlist el objeto playlist
      */
     void deleteByplaylist(Playlist playlist);
+
+    /**
+     * Metodo para eliminar una cancion de la playlist
+     * @param playlist el objeto playList
+     * @param cancion el objeto cancion
+     */
+    void deleteByplaylistAndCancion(Playlist playlist, Cancion cancion);
 }
