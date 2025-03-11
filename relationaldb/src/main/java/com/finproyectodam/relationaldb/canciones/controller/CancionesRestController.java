9b6548547f434c503951b7fd1b6211205ea91393
cristian <cregueiromartinez@danielcastelao.org>
2025-03-11 17:41:33 +0100

    rest controller canciones lista

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/canciones/controller/CancionesRestController.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/canciones/controller/CancionesRestController.java
@@ -1,4 +1,50 @@
 package com.finproyectodam.relationaldb.canciones.controller;
 
+import com.finproyectodam.relationaldb.canciones.service.CancionesService;
+import com.finproyectodam.relationaldb.model.entitys.Cancion;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * RestController de canciones
+ * @author cristian
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/relationaldb/spotify/canciones")
 public class CancionesRestController {
+
+    //servicio de las canciones
+    private final CancionesService cancionesService;
+
+    /**
+     * Constructor de la clase
+     * @param cancionesService el servicio de las canciones
+     */
+    public CancionesRestController(CancionesService cancionesService) {
+        this.cancionesService = cancionesService;
+    }
+
+    /**
+     * Metodo para obtener un listado de las canciones de spotify
+     * @return la lista de canciones o null
+     */
+    @GetMapping("/info")
+    public ResponseEntity<List<Cancion>> getAllSongsrestController() {
+        try{
+            List<Cancion> cancionList = cancionesService.getAllSongsService();
+            if(cancionList != null) {
+                return ResponseEntity.ok(cancionList);
+            }
+            else{
+                return ResponseEntity.notFound().build();
+            }
+        }catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
