5fb8f65491fbf0c1f4acb2761d5ae48859783e2e
cristian <cregueiromartinez@danielcastelao.org>
2025-03-11 17:37:45 +0100

    servicio de canciones obtener lista

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/canciones/service/CancionesService.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/canciones/service/CancionesService.java
@@ -1,4 +1,39 @@
 package com.finproyectodam.relationaldb.canciones.service;
 
+import com.finproyectodam.relationaldb.model.entitys.Cancion;
+import com.finproyectodam.relationaldb.repository.CancionesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Servicio de canciones
+ * @author cristian
+ * @version 1.0
+ */
+@Service
 public class CancionesService {
+
+    //repositorio de las canciones
+    private final CancionesRepository cancionesRepository;
+
+    /**
+     * Constructor de la clase
+     * @param cancionesRepository el repositorio de las canciones
+     */
+    public CancionesService(CancionesRepository cancionesRepository) {
+        this.cancionesRepository = cancionesRepository;
+    }
+
+    /**
+     * Metodo para obtener una lista de todas las canciones de la base de datos
+     * @return la lista de canciones o null
+     */
+    public List<Cancion> getAllSongsService(){
+        List<Cancion> cancionList = cancionesRepository.findAll();
+        if(cancionList.isEmpty()){
+            return null;
+        }
+        return cancionList;
+    }
 }
