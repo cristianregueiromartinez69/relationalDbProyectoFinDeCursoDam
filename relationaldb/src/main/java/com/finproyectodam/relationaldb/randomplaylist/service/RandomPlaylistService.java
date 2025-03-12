0317e62d2ca593ed48f0c5e421c52de27322e2fa
cristian <cregueiromartinez@danielcastelao.org>
2025-03-12 17:08:02 +0100

    haciendo metodo repo usuarios get by id

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/randomplaylist/service/RandomPlaylistService.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/randomplaylist/service/RandomPlaylistService.java
@@ -1,9 +1,15 @@
 package com.finproyectodam.relationaldb.randomplaylist.service;
 
+import com.finproyectodam.relationaldb.model.entitys.Playlist;
+import com.finproyectodam.relationaldb.model.entitys.Usuario;
+import com.finproyectodam.relationaldb.playlist.service.PlayListService;
 import com.finproyectodam.relationaldb.repository.PlayListsRepository;
 import com.finproyectodam.relationaldb.repository.UsuariosRepository;
 import com.finproyectodam.relationaldb.usuarios.token.UsersTokens;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RandomPlaylistService {
@@ -11,12 +17,20 @@ public class RandomPlaylistService {
     private final UsuariosRepository usuariosRepository;
     private final PlayListsRepository playlistsRepository;
     private final UsersTokens usersTokens;
+    private final PlayListService playListService;
+    private static final Integer ID_USER_ADMIN_RANDOM_PLAYLIST = 8;
 
-    public RandomPlaylistService(UsuariosRepository usuariosRepository, PlayListsRepository playlistsRepository, UsersTokens usersTokens) {
+    public RandomPlaylistService(UsuariosRepository usuariosRepository, PlayListsRepository playlistsRepository, UsersTokens usersTokens, PlayListService playListService) {
         this.usuariosRepository = usuariosRepository;
         this.playlistsRepository = playlistsRepository;
         this.usersTokens = usersTokens;
+        this.playListService = playListService;
     }
+
+    private List<Playlist> getAllAdminPlaylists() {
+        List<Playlist> playlists = new ArrayList<>();
+    }
+
 
 
 }
