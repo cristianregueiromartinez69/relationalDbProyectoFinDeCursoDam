0c4badb8b53f9b56b31472075ec26496387e4c11
Cristian <cregueiromartinez@danielcastelao.org>
2025-03-27 11:06:42 +0100

    esqueleto historial canciones escuchadas añadidas

---
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/historial/controller/HistorialRestController.java
@@ -0,0 +1,4 @@
+package com.finproyectodam.relationaldb.historial.controller;
+
+public class HistorialRestController {
+}
---
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/historial/service/HistorialService.java
@@ -0,0 +1,4 @@
+package com.finproyectodam.relationaldb.historial.service;
+
+public class HistorialService {
+}
