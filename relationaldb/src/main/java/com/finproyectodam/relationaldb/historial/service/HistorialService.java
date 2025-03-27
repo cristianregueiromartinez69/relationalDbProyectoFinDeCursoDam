a06b63c7b4eb33f87886337c818e8c9328fbeb83
Cristian <cregueiromartinez@danielcastelao.org>
2025-03-27 12:26:57 +0100

    pasando a rest controller de historial

--- relationaldb/src/main/java/com/finproyectodam/relationaldb/historial/service/HistorialService.java
+++ relationaldb/src/main/java/com/finproyectodam/relationaldb/historial/service/HistorialService.java
@@ -8,20 +8,34 @@ import com.finproyectodam.relationaldb.usuarios.token.UsersTokens;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+/**
+ * Servicio de guardado de canciones en el historial
+ * @author cristian && Joel
+ * @version 1.0
+ */
 @Service
 public class HistorialService {
 
+    //atributos de clase
     private final RedisTemplate<Integer, Object> redisTemplate;
     private final PlayListService playListService;
     private final UsersTokens usersTokens;
 
-
+    /**
+     * Constructor de la clase
+     * @param redisTemplate el servicio de redis
+     * @param playListService el servicio de playlist
+     */
     public HistorialService(RedisTemplate<Integer, Object> redisTemplate, PlayListService playListService) {
         this.redisTemplate = redisTemplate;
         this.playListService = playListService;
         this.usersTokens = new UsersTokens();
     }
 
+    /**
+     * Metodo que guarda el historial de una cancion en redis
+     * @param idCancion el id de la cancion
+     */
     public void saveSongHistorial(Integer idCancion) {
         Usuario usuario1 = playListService.getCurrentUser(usersTokens.getUserTokens());
         if (usuario1 == null) {
@@ -29,10 +43,20 @@ public class HistorialService {
         }
         else{
             Integer idUsuario = usuario1.getId();
+            if(checkLimitSongRedis(idUsuario)) {
+                redisTemplate.opsForList().remove(idUsuario, 1, idCancion);
+                redisTemplate.opsForList().leftPush(idUsuario, idCancion);
+            }
         }
     }
 
+    /**
+     * Metodo para comprobar si tienes más de 10 canciones en el historial
+     * @param idUsuario el id del usuario que es la clave
+     * @return true o false dependiendo de si lo superas o no
+     */
     private boolean checkLimitSongRedis(Integer idUsuario) {
-        return redisTemplate.opsForList().size(idUsuario) < 10;
+        Long size = redisTemplate.opsForList().size(idUsuario);
+        return size != null && size < 10;
     }
 }
